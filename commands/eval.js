314211050a00880007040457673547cedd3e2727
EPICZEUS <sbepiczeus@aol.com>
2017-05-07 16:02:26 -0400

    Fix improper time format

--- commands/eval.js
+++ commands/eval.js
@@ -13,7 +13,7 @@ async function update(message, promise, embed) {
 
 	console.log(done);
 
-	embed.addField("Awaited Promise", (done.length < 1000 ? `\`\`\`${done}\`\`\`` : "Promise return too long, logged") + `\n\nResolved in \`${end}\u03bcs\``);
+	embed.addField("Awaited Promise", (done.length < 1000 ? `\`\`\`${done}\`\`\`` : "Promise return too long, logged") + `\n\nResolved in \`${(end / 1000).toFixed(3)}\u03bcs\``);
 
 	message.edit(message.content, {embed});
 }
@@ -44,7 +44,7 @@ exports.run = (bot, message, args) => {
 		const embed = new Discord.RichEmbed()
 			.setTitle("**OUTPUT**")
 			.setDescription("```js\n" + evaled.replace(/`/g, "`\u200b").replace(new RegExp(`${bot.token}${bot.config.customsearch ? `|${bot.config.customsearch.token}|${bot.config.customsearch.id}` : ""}`, "g"), "[SECRET]") + "\n```")
-			.setFooter(`Runtime: ${runTime}\u03bcs`, "https://cdn.discordapp.com/attachments/286943000159059968/298622278097305600/233782775726080012.png")
+			.setFooter(`Runtime: ${(runTime / 1000).toFixed(3)}\u03bcs`, "https://cdn.discordapp.com/attachments/286943000159059968/298622278097305600/233782775726080012.png")
 			.setColor(24120);
 
 		message.edit(`**INPUT:** \`${code}\``, {embed}).then(async msg => {
