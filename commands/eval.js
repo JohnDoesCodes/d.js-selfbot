801ee4d082c2e942b55cff7140dcb9f60eb4ce65
EPICZEUS <sbepiczeus@aol.com>
2017-05-07 16:11:18 -0400

    Made eval not error on output too long

--- commands/eval.js
+++ commands/eval.js
@@ -43,7 +43,7 @@ exports.run = (bot, message, args) => {
 
 		const embed = new Discord.RichEmbed()
 			.setTitle("**OUTPUT**")
-			.setDescription("```js\n" + evaled.replace(/`/g, "`\u200b").replace(new RegExp(`${bot.token}${bot.config.customsearch ? `|${bot.config.customsearch.token}|${bot.config.customsearch.id}` : ""}`, "g"), "[SECRET]") + "\n```")
+			.setDescription(evaled.length < 2036 ? "```js\n" + evaled.replace(/`/g, "`\u200b").replace(new RegExp(`${bot.token}${bot.config.customsearch ? `|${bot.config.customsearch.token}|${bot.config.customsearch.id}` : ""}`, "g"), "[SECRET]") + "\n```" : "Output too long.\nSaved to console")
 			.setFooter(`Runtime: ${(runTime / 1000).toFixed(3)}\u03bcs`, "https://cdn.discordapp.com/attachments/286943000159059968/298622278097305600/233782775726080012.png")
 			.setColor(24120);
 
