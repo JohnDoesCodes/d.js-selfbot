2d7abe311921a4aaf32d2a45f00ca5acb1ad4cf1
EPICZEUS <sbepiczeus@aol.com>
2017-05-11 02:25:52 -0400

    Log cleared game properly

--- src/commands/setgame.js
+++ src/commands/setgame.js
@@ -2,7 +2,7 @@ exports.run = (bot, message, args) => {
     const game = args.length ? args.join(" ") : null;
 
     bot.user.setGame(game)
-		.then(() => console.log(`New game set to ${args.join(" ")}.`))
+		.then(() => console.log(game ? `New game set to '${game}'` : "Game cleared!"))
 		.catch(console.error);
 };
 
