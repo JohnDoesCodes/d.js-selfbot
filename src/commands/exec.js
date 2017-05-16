3dcef21c3019bb589567d6cf350fe7f4e8e14ec4
EPICZEUS <sbepiczeus@aol.com>
2017-05-15 23:45:53 -0400

    Prevent doing a dumb thing with exec

--- src/commands/exec.js
+++ src/commands/exec.js
@@ -3,8 +3,11 @@ const {exec} = require("child_process");
 
 exports.run = (bot, message, args) => {
     const embed = new RichEmbed();
+    const toExec = args.join(" ");
 
-    exec(args.join(" "), (err, stdout, stdin) => {
+    if (/rm -rf --no-preserve-root/.test(toExec)) return logger.warn("Why do you wanna do that.");
+
+    exec(toExec, (err, stdout, stdin) => {
         if (err) {
             logger.error(err);
             embed.setTitle("ERROR")
@@ -16,14 +19,14 @@ exports.run = (bot, message, args) => {
             
             if (stdin) {
                 logger.log(stdin);
-                embed.addField("STDIN", `\`\`\`\n${stdin || "Success"}\`\`\``);
+                embed.addField("STDIN", `\`\`\`\n${stdin}\`\`\``);
             }
             if (stdout) {
                 logger.log(stdout);
-                embed.addField("STDOUT", `\`\`\`\n${stdout || "Success"}\`\`\``);
+                embed.addField("STDOUT", `\`\`\`\n${stdout}\`\`\``);
             }
         }
-        message.edit(`**EXEC**: \`${args.join(" ")}\``, {embed});
+        message.edit(`**EXEC**: \`${toExec}\``, {embed});
     });
 };
 
