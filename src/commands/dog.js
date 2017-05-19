54614e2a3b3d4b8574bfa6c45ac84270219b64fd
EPICZEUS <sbepiczeus@aol.com>
2017-05-19 18:08:12 -0400

    Add dog command

---
+++ src/commands/dog.js
@@ -0,0 +1,15 @@
+const request = require("snekfetch");
+
+exports.run = (bot, message) => {
+    request.get("https://random.dog/woof.json").then(res => {
+        message.edit(res.body.url);
+    });
+};
+
+exports.name = "dog";
+exports.type = "general";
+exports.description = "Gets a random dog image";
+exports.use = "dog";
+exports.aliases = [
+    "doggo"
+];
